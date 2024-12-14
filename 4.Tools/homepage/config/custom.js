76e1a617d59a6ca4fd6393c993dc19bd0cebfc58
GodD6366 <daichangchun6366@gmail.com>
2024-12-14 00:36:06 +0000

    测试dokploy

---
+++ 4.Tools/homepage/config/custom.js
@@ -0,0 +1,12 @@
+// setTimeout(() => {
+  window.difyChatbotConfig = {
+    token: 'Y9qVHigkI6x1qirf',
+    baseUrl: 'https://dify.godd.site:6443',
+  };
+
+  var script = document.createElement('script');
+  script.src = 'https://dify.godd.site:6443/embed.min.js';
+  script.id = 'Y9qVHigkI6x1qirf';
+  // script.defer = true;
+  document.body.appendChild(script);
+// }, 1000);
